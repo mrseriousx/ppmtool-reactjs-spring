10822edd88a8e2bdb199bf823aed5030e88d927b
Murphy <haihuynhng@gmail.com>
2020-03-08 12:53:41 +0700

    first commit

---
+++ PPMToolSpring/src/test/java/com/example/PPMToolSpring/PpmToolSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PPMToolSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
